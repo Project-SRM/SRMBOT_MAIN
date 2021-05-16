ccfb9f926047e2670ae741fbd6d12b77b97a48d4
Vijeta Sarkar <vijeta.sarkar201099@gmail.com>
2021-05-16 20:48:33 +0530

    Updated config.js

--- src/chatbot/config.js
+++ src/chatbot/config.js
@@ -104,11 +104,14 @@ const config = {
     //   }
     // ),
     
-  createChatBotMessage(`Fill the below form to proceed`,
+  createChatBotMessage(`Fill the below form to contact us`,
   { widget :"form"}
   ),
-  createChatBotMessage(`Hi there ! How can I help you ?`,
-  { widget :"overview"}
+  // createChatBotMessage(`Hi there ! How can I help you ?`,
+  // { widget :"overview"}
+  // )
+  createChatBotMessage(`Do you have more queries?`,
+  { widget :"queries"}
   )
 
 ],
